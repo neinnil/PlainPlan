a841ccf6744e88f28be5e29b18f167d5d1c2da7b
Park Sangjun <sjpark0820@sntek.com>
2018-11-05 02:25:49 +0000

    add the  ProjSummary component.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- img alt="Vue logo" src="./assets/logo.png" //-->
     <!-- HelloWorld msg="Welcome to Your Vue.js App"/ //-->
 		<User />
+		<ProjSummary />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 import User from './components/User.vue'
+import ProjSummary from './components/ProjSummary.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
-		User
+ //   HelloWorld,
+		User,
+		ProjSummary,
   }
 }
 </script>
